a9da040775756fa3624d25d92803e8e30a14e3be
Ahmed Al-Halwagy <aghalwagy@gmail.com>
2016-10-13 19:22:47 +0200

    Initial implementation ..

--- module1/assignments/assignment1/scripts/app.js
+++ module1/assignments/assignment1/scripts/app.js
@@ -1,11 +1,44 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	var app = angular.module('LunchCalculatorApp', []);
+    var app = angular.module('LunchCalculatorApp', []);
 
+    app.controller('LunchCalculatorController', LunchCalculatorController);
 
-	LuncCalculatorController.$inject = ['$scope'];
-	function LuncCalculatorController($scope){
+    LunchCalculatorController.$inject = ['$scope'];
 
-	};
+    function isEmptyString(string) {
+        return string.trim().length == 0;
+    }
+
+    function splitText(text, separator) {
+    	if (isEmptyString(text)) return [];
+
+        return text.split(separator, function(e) {
+            !isEmptyString(e);
+        });
+    }
+
+    function LunchCalculatorController($scope) {
+        $scope.lunchText = "";
+        $scope.lunchEvaluation = "";
+
+        $scope.calculateLunch = function() {
+        	
+        	// split entries and exclude the empty ones
+            var entries = splitText($scope.lunchText, ",");
+
+            if (entries.length == 0) {
+                $scope.lunchEvaluation = "Please enter data first";
+                return;
+            }            
+
+            if (entries.length <= 3) {
+                $scope.lunchEvaluation = "Enjoy!";
+            } else {
+                $scope.lunchEvaluation = "Too Much!";
+            }
+
+        };
+    };
 })();
