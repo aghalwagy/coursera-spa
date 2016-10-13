2ee52b89f50d19766e5a6ebf51d797b9712137d4
Ahmed Al-Halwagy <aghalwagy@gmail.com>
2016-10-13 17:40:24 +0200

    added starting point for assignment 1

---
+++ module1/assignments/assignment1/scripts/app.js
@@ -0,0 +1,11 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('LunchCalculatorApp', []);
+
+
+	LuncCalculatorController.$inject = ['$scope'];
+	function LuncCalculatorController($scope){
+
+	};
+})();
