2a03ca8bba9bc7d33693f11827f9fc1e15f4436a
Ahmed Al-Halwagy <aghalwagy@gmail.com>
2016-10-17 23:53:00 +0200

    The function passed to setTimeout will be processed on an event queue different than the one used by angular; so the dirty checking process might not work! That's simply because nobody told angular to run its digest cycle.

--- watchers-and-digest-cycle/digest-apply/app.js
+++ watchers-and-digest-cycle/digest-apply/app.js
@@ -13,6 +13,18 @@
       console.log('Counter incremented.');
       $scope.counter++;
     };
+
+    // uses a time out to delay the execution for one second
+    $scope.incrementDelayed = function () {
+
+      // the function passed to setTimeout will be processed on an event queue
+      // different than the one used by angular; so the dirty checking process might not work
+      // simply nobody told angular to run its digest cycle.
+      setTimeout(function () {
+        console.log('Counter incrementDelayed');
+        $scope.counter++;
+      }, 1000);
+    };
   }
 
 }());
