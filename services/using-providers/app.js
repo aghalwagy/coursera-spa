01482d92a1893e6ed0400bf96226d2fb3d9642e8
Ahmed Al-Halwagy <aghalwagy@gmail.com>
2016-10-23 12:31:21 +0200

    the same shopping list app; this time using providers to create the services

---
+++ services/using-providers/app.js
@@ -0,0 +1,84 @@
+(function() {
+    'use strict';
+
+    var app = angular.module('ShoppingList', [])
+        .controller('ShoppingController', shoppingController)
+        .provider('ShoppingService', ShoppingServiceProvider);
+
+
+    shoppingController.$inject = ['ShoppingService'];
+
+    function shoppingController(service) {
+        var ctrl = this;
+
+        ctrl.name = "";
+        ctrl.quantity = "";
+        ctrl.errorMessage = "";
+
+        ctrl.getItems = function() {
+            return service.getItems();
+        }
+
+        ctrl.addItem = function() {
+            try {
+                service.addItem(ctrl.name, ctrl.quantity);
+            } catch (err) {
+                ctrl.errorMessage = err.message;
+            }
+        }
+
+        ctrl.removeItem = function(index) {
+            return service.removeItem(index);
+        }
+
+        ctrl.removeAll = function() {
+            return service.removeAll();
+        }
+    }
+
+    function ShoppingService(limit) {
+
+        var service = this;
+
+        var items = [];
+
+        service.addItem = function(name, quantity) {
+            console.log(items);
+            if (limit === undefined || (limit != undefined && limit > items.length)) {
+                items.push({
+                    name: name,
+                    quantity: quantity
+                });
+            } else {
+                throw new Error('Max limit(' + limit + ') reached.');
+            }
+        }
+
+        service.getItems = function() {
+            return items;
+        }
+
+        service.removeItem = function(index) {
+            items.splice(index, 1);
+        }
+
+        service.removeAll = function() {
+            items.splice(0, items.length);
+        }
+    }
+
+    function ShoppingServiceProvider() {
+        var provider = this;
+
+        provider.defaults = {
+            max: 4
+        };
+
+        provider.$get = function() {
+            var shoppingList = new ShoppingService(provider.defaults.max);
+
+            return shoppingList;
+        }
+    }
+
+}());
