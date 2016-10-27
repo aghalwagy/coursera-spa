eac54168a3e504eba98e25140c5c007402384574
Ahmed Al-Halwagy <aghalwagy@gmail.com>
2016-10-27 06:38:13 +0200

    Working the examples of lecture 30 .. The main lesson here is the use of '&' when defining functions on the main controller to be passed by reference. And therefore, 'this' inside the function will point to the real controller, in our case 'ShoppingListController', and no the Directive's controller, in our case 'ShoppingListDirectiveController'.
    ** HINT: using arrow functions, supposedly solves the 'this' problem
    for it's interpreted lexically

---
+++ directives/scope-isolates-and-function-closures/app.js
@@ -0,0 +1,76 @@
+(function() {
+    'use strict';
+
+    angular.module('DirectivesApp', [])
+        .controller('ShoppingListController', ShoppingListController)
+        .service('ShoppingListService', ShoppingListService)
+        .directive('shoppingList', ShoppingListDirective);
+
+    function ShoppingListDirective() {
+        var ddo = {
+            templateUrl: 'templates/shoppinglist.html',
+            controller: ShoppingListDirectiveController,
+            controllerAs: 'list',
+            bindToController: true,
+            scope: {
+                items: '<',
+                myTitle: '@title',
+                badRemove: '=',
+                goodRemove: '&'
+            }
+        };
+
+        return ddo;
+    }
+
+    function ShoppingListDirectiveController() {
+        var list = this;
+        list.vodkaInList = () => {
+          // console.log(this);
+            //   for (var i = 0; i < list.items.length; i++) {
+            //     if (list.items[i].name.toLowerCase().indexOf('vodka') !== -1) {
+            //         return true;
+            //     }
+            // }
+            // return false;
+        };
+    }
+
+    ShoppingListController.$inject = ['ShoppingListService'];
+    function ShoppingListController(service) {
+        var ctrl = this;
+        ctrl.itemName = "";
+        ctrl.itemQuantity = "";
+        ctrl.title = "Checkout those directives y'all";
+        ctrl.addItem = () => {
+          service.addItem(ctrl.itemName, ctrl.itemQuantity);
+        }
+
+        ctrl.items = service.getItems();
+
+        ctrl.removeItem = function (index) {
+          console.log('this is:', this);
+          service.removeItem(index);
+        };
+    }
+
+    function ShoppingListService() {
+        var svc = this;
+
+        svc.items = [];
+        svc.getItems = () => svc.items;
+
+        svc.addItem = (name, quantity) => {
+            svc.items.push({
+                name: name,
+                quantity: quantity
+            });
+        };
+
+        svc.removeItem = (index) => {
+            // console.log('this is: ', this);
+            svc.items.splice(index, 1);
+        };
+    }
+
+}());
